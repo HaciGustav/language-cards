9b2ec61482b9f6e4f62595f740e4c4f59c729923
MehmetFK <mehmetfkuruldak@gmail.com>
2022-11-15 09:46:14 +0100

    structured

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from './components/Header';
+import Card from './components/Card';
+import Container from 'react-bootstrap/Container';
 function App() {
-    return <h1>APP</h1>;
+    return (
+        <div>
+            <Container className="text-center">
+                <Header />
+                <Card />
+            </Container>
+        </div>
+    );
 }
 
 export default App;
